6b701e7d0acacae118208efdcf8c94e435c834b7
Virginia <virlli_nia@hotmail.com>
2022-02-21 17:58:26 +0100

    weather card

---
+++ src/app/components/weather-card/weather-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-card',
+  templateUrl: './weather-card.component.html',
+  styleUrls: ['./weather-card.component.scss']
+})
+export class WeatherCardComponent implements OnInit {
+  temp: string = '15';
+  city: string = 'Heidenheim'
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
